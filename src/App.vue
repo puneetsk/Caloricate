5a03ad299fc0917d6d63d310c19001a22b160d23
Puneet Kolathaya <pkolatha@hawk.iit.edu>
2017-11-07 22:48:28 -0600

    Switching to Bootstrap V4

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,99 @@
 <template>
-  <div id="app" class="container">
-    <div class="page-header">
-      <h1>Welcome to Calorie Counter</h1>
-    </div>
+  <div id="app" class="container-fluid">
 
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h2>Add Food</h2>
+
+
+      <header>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 py-4">
+              <h4 class="text-white">About</h4>
+              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+            </div>
+            <div class="col-sm-4 py-4">
+              <h4 class="text-white">Contact</h4>
+              <ul class="list-unstyled">
+                <li><a href="#" class="text-white">Follow on Twitter</a></li>
+                <li><a href="#" class="text-white">Like on Facebook</a></li>
+                <li><a href="#" class="text-white">Email me</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div class="panel-body">
-        <form id="form" class="form-inline" v-on:submit.prevent="addFood">
-          <div class="form-group">
-            <label for="fooditem">Food Item </label>
-            <input type="text" id="footitem" class="form-control" v-model="newFood.Item"></input>
-          </div>
-
-          <div class="form-group">
-            <label for="foodcalorie">Calories </label>
-            <input type="text" id="foodcalorie" class="form-control" v-model="newFood.Calories"></input>
-          </div>
-
-          <div class="form-group">
-            <label for="foodfat">Fat </label>
-            <input type="text" id="foodfat" class="form-control" v-model="newFood.Fat"></input>
-          </div>
-
-          <div class="form-group">
-            <label for="foodprotein">Protein</label>
-            <input type="text" id="foodprotein" class="form-control" v-model="newFood.Protein"></input>
-          </div>
-
-          <input type="submit" class="btn btn-primary" value="Add Food Items">
-        </form>
+      <div class="navbar navbar-dark bg-dark">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand"><img src="./assets/drink.png" class="img-responsive" width="60"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
       </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h2>Calculated Calories</h2>
-      </div>
+    </header>
 
-      <div class="panel-body">
-        <table class="table table-stripped">
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Calories</th>
-              <th>Fat</th>
-              <th>Protien</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="food in Foods">
-              <td>{{ food.Item }}</td>
-              <td>{{ food.Calories}}g</td>  
-              <td>{{ food.Fat }}g</td>
-              <td>{{ food.Protein }}g</td>
-              <td> <span class="glyphicon glyphicon-trash" v-on:click="removeItem(food)"></span></td>
-            </tr>
-          </tbody>
-        </table>
+
+
+    <div class="container">    
+      <div class="card">
+        <div class="card-header">
+          <h2>Add Food</h2>
+        </div>
+
+        <div class="card-body">
+          <form id="form" class="form-inline" v-on:submit.prevent="addFood">
+            <div class="col">
+          
+              <input type="text" id="footitem" class="form-control" v-model="newFood.Item"></input>
+            </div>
+
+            <div class="col">
+             
+              <input type="text" id="foodcalorie" class="form-control" v-model="newFood.Calories"></input>
+            </div>
+
+            <div class="col">
+             
+              <input type="text" id="foodfat" class="form-control" v-model="newFood.Fat"></input>
+            </div>
+
+            <div class="col">
+              
+              <input type="text" id="foodprotein" class="form-control" v-model="newFood.Protein"></input>
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="Add Food Items">
+          </form>
+        </div>
+      </div>
+   
+      <div class="card">
+        <div class="card-header">
+          <h2>Calculated Calories</h2>
+        </div>
+
+        <div class="card-body">
+          <table class="table table-stripped">
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Calories</th>
+                <th>Fat</th>
+                <th>Protien</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="food in Foods">
+                <td>{{ food.Item }}</td>
+                <td>{{ food.Calories}}g</td>  
+                <td>{{ food.Fat }}g</td>
+                <td>{{ food.Protein }}g</td>
+                <td> <span class="glyphicon glyphicon-trash" v-on:click="removeItem(food)"></span></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -117,15 +151,19 @@ let dataref= db.ref('Foods'); /* Referencing the category*/
 </script>
 
 <style>
+body{background: #7BB0A6;}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
-
+.page-header{color: #fff;}
 table td, th{text-align: center;}
 .btn{margin: 20px;}
+.container-fluid{
+  margin: 0; padding: 0;
+}
 </style>
