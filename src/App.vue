2c7774eed28d197059c47a5cb35bf7ba659afe43
Puneet Kolathaya <pkolatha@hawk.iit.edu>
2017-11-16 16:08:28 -0600

    About us changes to the site

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           <div class="row">
             <div class="col-sm-8 py-4">
               <h4 class="text-white">About</h4>
-              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+              <p class="text-muted">Caloricate is an application built to monitor the Calorie Consumption in a day. You can basically search for food that you consumed in a day and Caloricate will help you add the number of calories consumed. Very easy to use application, adding the calories with a click of a button.</p>
             </div>
             <div class="col-sm-4 py-4">
               <h4 class="text-white">Contact</h4>
